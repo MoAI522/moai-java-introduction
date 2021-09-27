72ac753efa4cde5bc5eeadbf3f3a2750d0e59b00
MoAI522 <urotanre522@gmail.com>
2021-09-27 02:58:35 +0000

    language1.HelloWorld

---
+++ app/language1/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package language1.HelloWorld;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    int studentID = 6320027;
+    String name = "川田廉太郎";
+    System.out.printf("%d %s\n", studentID, name);
+  }
+}
