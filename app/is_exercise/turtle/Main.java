18a7412072703d18a19d91b49832df8c3c5f4825
MoAI <urotanre522@gmail.com>
2021-11-20 04:57:15 +0000

    fix

--- app/is_exercise/turtle/Main.java
+++ app/is_exercise/turtle/Main.java
@@ -14,21 +14,21 @@ public class Main extends Application {
 
     Operation.load();
 
-    // Turtle t1 = new Turtle(200, 200);
-    // Operation.square(t1);
-    // tm.add(t1);
+    Turtle t1 = new Turtle(200, 200);
+    Operation.square(t1);
+    tm.add(t1);
 
-    // Turtle t2 = new Turtle(400, 100);
-    // Operation.triangles(t2, 40, 2, 20);
-    // tm.add(t2);
+    Turtle t2 = new Turtle(400, 100);
+    Operation.triangles(t2, 40, 2, 20);
+    tm.add(t2);
 
-    // Turtle t3 = new Turtle(400, 400);
-    // Operation.polygons(t3, 10, 80);
-    // tm.add(t3);
+    Turtle t3 = new Turtle(400, 400);
+    Operation.polygons(t3, 10, 80);
+    tm.add(t3);
 
-    Turtle t4 = new Turtle(30, 100);
-    Operation.symbols(t4, 200);
-    tm.add(t4);
+    // Turtle t4 = new Turtle(30, 100);
+    // Operation.symbols(t4, 200);
+    // tm.add(t4);
 
     tm.draw(wm.getGC());
     wm.setOnRender(() -> {
