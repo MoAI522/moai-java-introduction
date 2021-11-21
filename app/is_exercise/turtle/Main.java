1d394620d67182b32d6617d5efc64414ecaea23d
MoAI522 <urotanre522@gmail.com>
2021-11-21 18:29:16 +0000

    fix

--- app/is_exercise/turtle/Main.java
+++ app/is_exercise/turtle/Main.java
@@ -1,5 +1,6 @@
 import drag.DragManager;
 import javafx.application.Application;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import manager.TurtleManager;
 import operation.Operation;
@@ -7,6 +8,8 @@ import turtle.Turtle;
 import window.WindowManager;
 
 public class Main extends Application {
+  private static String[] args;
+
   @Override
   public void start(Stage st) {
     WindowManager wm = new WindowManager(st);
@@ -14,31 +17,70 @@ public class Main extends Application {
 
     Operation.load();
 
-    Turtle t1 = new Turtle(200, 200);
-    Operation.square(t1);
-    tm.add(t1);
-
-    Turtle t2 = new Turtle(400, 100);
-    Operation.triangles(t2, 40, 2, 20);
-    tm.add(t2);
-
-    Turtle t3 = new Turtle(400, 400);
-    Operation.polygons(t3, 10, 80);
-    tm.add(t3);
-
-    // Turtle t4 = new Turtle(30, 100);
-    // Operation.symbols(t4, 200);
-    // tm.add(t4);
+    boolean isNothing = true;
+    for (int i = 0; i < args.length; i++) {
+      switch (args[i]) {
+      case "0": {
+        Turtle t = new Turtle(200, 200);
+        t.penColor(Color.BLUE);
+        Operation.square(t);
+        tm.add(t);
+        isNothing = false;
+        break;
+      }
+      case "1": {
+        Turtle t = new Turtle(200, 200);
+        t.penColor(Color.RED);
+        Operation.triangles(t, 40, 2, 20);
+        tm.add(t);
+        isNothing = false;
+        break;
+      }
+      case "2": {
+        Turtle t = new Turtle(200, 300);
+        t.penColor(Color.YELLOWGREEN);
+        Operation.polygons(t, 10, 80);
+        tm.add(t);
+        isNothing = false;
+        break;
+      }
+      case "3": {
+        Turtle t = new Turtle(200, 200);
+        t.penColor(Color.GREEN);
+        Operation.tusSymbol(t, 200);
+        tm.add(t);
+        isNothing = false;
+        break;
+      }
+      case "4": {
+        Turtle t = new Turtle(30, 100);
+        Operation.symbols(t, 200);
+        tm.add(t);
+        isNothing = false;
+        break;
+      }
+      }
+    }
 
     tm.draw(wm.getGC());
-    wm.setOnRender(() -> {
-      tm.draw(wm.getGC());
-    });
+    if (isNothing) {
+      wm.setOnRender(() -> {
+        wm.getGC().setStroke(Color.BLACK);
+        wm.getGC().strokeText(
+            "Nothing is selected to draw.\nPlease select something by command-line arguments.\n0:square\n1:triangles\n2:polygons\n3:tus-symbol\n4:three-tus-symbols",
+            0, 20);
+      });
+    } else {
+      wm.setOnRender(() -> {
+        tm.draw(wm.getGC());
+      });
+    }
 
     DragManager dm = new DragManager(tm, wm);
   }
 
   public static void main(String[] args) {
+    Main.args = args;
     launch(args);
   }
 }
